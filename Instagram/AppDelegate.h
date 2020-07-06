81f459ac143b7bcd91d40bc5f99623317984fdf9
cryptonon <aayushphuyal0074@gmail.com>
2020-07-06 17:59:16 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Aayush Mani Phuyal on 7/6/20.
+//  Copyright © 2020 Aayush Phuyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Instagram
+//
+//  Created by Aayush Mani Phuyal on 7/6/20.
+//  Copyright © 2020 Aayush Phuyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
